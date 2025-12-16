26128b7bd6ccfca058fe6387ee48db35541d94f1
fscarmen <62703343+fscarmen@users.noreply.github.com>
2025-12-16 20:47:43 +0800

    Update n.sh

--- n.sh
+++ n.sh
@@ -5,12 +5,11 @@ CURL_ARGS=$2
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x6*4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 
 MediaUnlockTest_Netflix() {
-  local RESULT_1=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792" 2>&1 | awk '/curl:/{print}/og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"([^"]+)"/,m)){c++;if(c==2){print "requestCountry:",m[1]}$0=substr($0,RSTART+RLENGTH)}}')
+  local RESULT_1=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/81280792" 2>&1 | awk '/curl:/{print} /og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"[^"]+"/)){c++;s=substr($0,RSTART,RLENGTH);sub(/.*"id":"*/,"",s);sub(/".*/,"",s);if(c==2)print "requestCountry:",s;$0=substr($0,RSTART+RLENGTH)}}')
 
   grep -q 'curl:' <<< "$RESULT_1" && return 2
 
-  local RESULT_2=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" 2>&1 | awk '/curl:/{print}/og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"([^"]+)"/,m)){c++;if(c==2){print "requestCountry:",m[1]}$0=substr($0,RSTART+RLENGTH)}}')
-
+  local RESULT_2=$(curl ${CURL_ARGS} ${MODE} --user-agent "${UA_Browser}" -SsL --max-time 10 --tlsv1.3 "https://www.netflix.com/title/70143836" 2>&1 | awk '/curl:/{print} /og:video/{print "og:video"}{while(match($0,/"requestCountry":\{"supportedLocales":\[[^]]+\],"id":"[^"]+"/)){c++;s=substr($0,RSTART,RLENGTH);sub(/.*"id":"*/,"",s);sub(/".*/,"",s);if(c==2)print "requestCountry:",s;$0=substr($0,RSTART+RLENGTH)}}')
   grep -q 'curl:' <<< "$RESULT_2" && return 2
 
   REGION_1=$(awk '/requestCountry/{print $NF}' <<< "$RESULT_1")
